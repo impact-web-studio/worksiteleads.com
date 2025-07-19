efbd512fb2aafca349a082460cb3f745c94225cc
kenny-nguyen <developedbykenny@gmail.com>
2025-07-19 22:47:19 +0700

    add schema to head

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,19 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap({
+			filter: (page) =>
+				![
+					'https://worksiteleads.com/privacy-policy',
+					'https://worksiteleads.com/terms-of-use',
+				].includes(page),
+			changefreq: 'weekly',
+			priority: 0.7,
+			lastmod: new Date(),
+		}),
+	],
 	site: 'https://worksiteleads.com',
 	vite: {
 		plugins: [tailwindcss()],
