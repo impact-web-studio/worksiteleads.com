9e6c18dbedf3511670193a24399a9b568f399841
kenny-nguyen <developedbykenny@gmail.com>
2025-07-19 22:28:16 +0700

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
